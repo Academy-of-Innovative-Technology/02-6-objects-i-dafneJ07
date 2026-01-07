2f00510ab098fce47b45fdf29742f584480480d2
Dafne <dafnej@nycstudents.net>
2026-01-07 14:03:26 -0500

    yay

--- script.js
+++ script.js
@@ -15,9 +15,8 @@ var college = {
     college: [
         "Queens College", "Stanford University", "Wharton School of the University of Pennsylvania"
     ],
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYC397Rqv6H33J1SO9gBy0VcntWIyjst6SHQ&s",
-     "https://cdn.prod.website-files.com/68cc7b28d9d17aba092f94c3/6914e9ae53c15ccb29d18da9_651f24e188972afb0147f4d4_6500e4ea2689ad6cf64cd358_School%252520Image%252520-%252520Wharton.png","https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Stanford_Oval_May_2011_panorama.jpg/960px-Stanford_Oval_May_2011_panorama.jpg",
-    wiki: "https://en.wikipedia.org/wiki/Elon_Musk"
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYC397Rqv6H33J1SO9gBy0VcntWIyjst6SHQ&s"
+   
 }
 var daf = {
     first :"Dafne",
@@ -26,5 +25,21 @@ var daf = {
     college: [
         "Brooklyn College", "John Jay Collage of Criminal Justice","York College"
     ],
-    img: "", "", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYC397Rqv6H33J1SO9gBy0VcntWIyjst6SHQ&s"
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYC397Rqv6H33J1SO9gBy0VcntWIyjst6SHQ&s"
+}
+var beckyg = {
+	first:"Rebbeca",
+	last: "Gomez",
+	year: "1997",
+	albums: [
+       "Esquemas", "Encuentros", "Mala Santa"
+	]
+
+}
+var maya = {
+	first:"Maya",
+	last:"Of The Tecas",
+	friends: [
+     "Zatz", "Chimi", "Rico"
+	]
 }
