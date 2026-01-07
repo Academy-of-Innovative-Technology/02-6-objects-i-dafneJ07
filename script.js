9401c1a3366ab2c48ed128638331149ef4b069cc
Dafne <dafnej@nycstudents.net>
2026-01-07 14:03:11 -0500

    wow

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ var theranos = {
 	img: "https://startupi.com.br/wp-content/uploads/2023/08/20160711elizabeth-holmes.jpg",
     wiki: "https://en.wikipedia.org/wiki/Elizabeth_Holmes"
 };
+var college = {
+    first: "Elon",
+    last: "Musk",
+    year: 1977,
+    college: [
+        "Queens College", "Stanford University", "Wharton School of the University of Pennsylvania"
+    ],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYC397Rqv6H33J1SO9gBy0VcntWIyjst6SHQ&s",
+     "https://cdn.prod.website-files.com/68cc7b28d9d17aba092f94c3/6914e9ae53c15ccb29d18da9_651f24e188972afb0147f4d4_6500e4ea2689ad6cf64cd358_School%252520Image%252520-%252520Wharton.png","https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Stanford_Oval_May_2011_panorama.jpg/960px-Stanford_Oval_May_2011_panorama.jpg",
+    wiki: "https://en.wikipedia.org/wiki/Elon_Musk"
+}
+var daf = {
+    first :"Dafne",
+    last: "Jimenez",
+    year: 2008,
+    college: [
+        "Brooklyn College", "John Jay Collage of Criminal Justice","York College"
+    ],
+    img: "", "", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYC397Rqv6H33J1SO9gBy0VcntWIyjst6SHQ&s"
+}
